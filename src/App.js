3ea3e356e9e80ec9741da71f5ba4f7aed6efa9af
Vadim Aidlin <vadim@pendo.io>
2022-11-06 17:45:00 +0200

    init app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Home from './components/Home.js';
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <Home />
+      </body>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+function Home(props) {
+    return (<h1>home</h1>);
+}
+
+export default Home;
