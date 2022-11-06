036727a5bf1aa5698db97321c9280145cec49d27
Vadim Aidlin <vadim@pendo.io>
2022-11-06 18:39:00 +0200

    added pendo initialize

--- src/App.js
+++ src/App.js
@@ -5,30 +5,31 @@ import React from 'react';
 
 class App extends React.Component {
   componentDidMount() {
-    // pendo.initialize({
-    //   visitor: {
-    //     id: 'VISITOR-UNIQUE-ID'   // Required if user is logged in, default creates anonymous ID
-    //     // email:        // Recommended if using Pendo Feedback, or NPS Email
-    //     // full_name:    // Recommended if using Pendo Feedback
-    //     // role:         // Optional
+    /* global pendo */
+    pendo.initialize({
+      visitor: {
+        id: 'VISITOR-UNIQUE-ID'   // Required if user is logged in, default creates anonymous ID
+        // email:        // Recommended if using Pendo Feedback, or NPS Email
+        // full_name:    // Recommended if using Pendo Feedback
+        // role:         // Optional
 
-    //     // You can add any additional visitor level key-values here,
-    //     // as long as it's not one of the above reserved names.
-    //   },
+        // You can add any additional visitor level key-values here,
+        // as long as it's not one of the above reserved names.
+      },
 
-    //   account: {
-    //     id: 'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
-    //     // name:         // Optional
-    //     // is_paying:    // Recommended if using Pendo Feedback
-    //     // monthly_value:// Recommended if using Pendo Feedback
-    //     // planLevel:    // Optional
-    //     // planPrice:    // Optional
-    //     // creationDate: // Optional
+      account: {
+        id: 'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
+        // name:         // Optional
+        // is_paying:    // Recommended if using Pendo Feedback
+        // monthly_value:// Recommended if using Pendo Feedback
+        // planLevel:    // Optional
+        // planPrice:    // Optional
+        // creationDate: // Optional
 
-    //     // You can add any additional account level key-values here,
-    //     // as long as it's not one of the above reserved names.
-    //   }
-    // });
+        // You can add any additional account level key-values here,
+        // as long as it's not one of the above reserved names.
+      }
+    });
   }
   render() {
     return (
